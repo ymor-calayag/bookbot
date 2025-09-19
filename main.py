e086a486f4bc3a812b2aa86463f1ccfcd880d2e4
ymor-calayag <jeffreyymor.calayag@gmail.com>
2025-09-19 08:56:40 +0800

    Remove hard-coded path file + add more books

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import sys
 from stats import get_book_text, count_words, count_characters, sort_dict
 
-book_text = get_book_text('books/frankenstein.txt')
-char_dict = count_characters(book_text)
-
 def main():
+    # sys.argv represents the arguments passed to the script, access it by index.
+    book_text = get_book_text(sys.argv[1])
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(book_text)} total words")
     print("--------- Character Count -------")
@@ -13,9 +14,16 @@ def main():
     print("============= END ===============")
 
 def print_char_report():
+    book_text = get_book_text(sys.argv[1])
+    char_dict = count_characters(book_text)
+
     sorted_list = sort_dict(char_dict)
     for entry in sorted_list:
         if entry['char'].isalpha():
             print(f"{entry['char']}: {entry['num']}")
 
-main()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>") 
+    sys.exit(1)
+else:
+    main()
